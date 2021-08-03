8029882a372bb91c9f25f2ecb4ed38a03583cb87
Anders Dahl Henriksen <ahe@proinvent.dk>
2021-08-03 16:29:40 +0200

    -Added tfds based data generator

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,9 +1,11 @@
+import random
 from pathlib import Path
 from shutil import copytree
 from random import shuffle
 from operator import itemgetter
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, ImageDataGenerator
 
 
 AUGMENT_DICT = {'width_shift_range': 30, 'height_shift_range': 30, 'rotation_range': 360, 'vertical_flip': True,
@@ -107,3 +109,114 @@ def partition_dataset(dataset_path, timestamp_as_unit=False, train_split=0.7, va
             [file_path.rename(dst_folder / file_path.name) for file_path in folder.glob(f"{stem}{'' if timestamp_as_unit else '.'}*")]
     [folder.rmdir() for folder in entries_to_move if list(folder.iterdir()) == []]
     print('Done')
+
+
+# --------------------------------- CODE BELOW HERE IS FOR TFDS DATA GENERATOR -----------------------------------------
+
+
+def get_data_for_classification_tfds(config, preprocess_input=None, augment_validation_data=False,
+                                     timestamp_as_unit=False, use_keras_aug=True, cache_data=True):
+    # Move to train / test directory
+    partition_dataset(config.data_folder, timestamp_as_unit, 1 - config.test_split_ratio, 0, config.test_split_ratio)
+    config.data_folder_train = str(config.data_folder / 'Train')
+    config.data_folder_test = str(config.data_folder / 'Test')
+
+    preprocess_input = preprocess_input or rescale
+    gens = {}
+    for type, data_folder in zip(['Train', 'Test'], [config.data_folder_train, config.data_folder_test]):
+        data_gen = DataGenerator(data_folder, config.input_shape)
+        ds = tf.data.Dataset.from_generator(data_gen, (tf.uint8, tf.float32), (config.input_shape, [data_gen.n_labels]))
+        if cache_data:
+            ds = ds.cache()
+        else:
+            print("Warning: Not caching dataset in memory, risk of decoding the every images over and over again. Consider caching to file")
+        ds = ds.batch(config.batch_size)
+        if type == 'Train' or augment_validation_data:
+            if use_keras_aug:
+                ds = ds.map(keras_augmentation_wrapper, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            else:
+                ds = ds.map(tf_augmentation, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds = ds.map(preprocess_input)
+        ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
+        ds.num_classes = data_gen.n_labels
+        ds.filepaths = data_gen.image_paths
+        gens[type] = ds
+    return gens['Train'], gens['Test']
+
+
+class DataGenerator:
+    def __init__(self, data_folder, input_shape, glob_pattern="*.png"):
+        self.image_paths = list(Path(data_folder).rglob(glob_pattern))
+        label_folders = sorted(f.name for f in Path(data_folder).glob("*") if f.is_dir())
+        self.input_shape = input_shape
+        self.label_int = {f: i for i, f in enumerate(label_folders)}
+        self.n_labels = len(label_folders)
+        self.color_mode = "grayscale" if input_shape[2] == 1 else 'rgb'
+        random.Random(42).shuffle(self.image_paths)
+        self.n = len(self.image_paths)
+        assert self.n, "DataGenerator could not find any samples"
+
+    def __call__(self):
+        for ip in self.image_paths:
+            img = load_img(ip, target_size=self.input_shape, color_mode=self.color_mode)
+            X = np.array(img)
+            X = X if X.ndim == 3 else X[:, :, None]
+            Y = self.label_int[ip.parent.name]
+            Y = tf.keras.utils.to_categorical(Y, self.n_labels)
+            yield X, Y
+
+
+def rescale(img, label):
+    return tf.cast(img, tf.float32) / 127.5 - 1.0, label
+
+
+def keras_augmentation(images):
+    if not hasattr(keras_augmentation, 'aug_gen'):
+        keras_augmentation.aug_gen = ImageDataGenerator(**AUGMENT_DICT)
+    return keras_augmentation.aug_gen.flow(images, batch_size=images.shape[0], shuffle=False)[0]
+
+
+def keras_augmentation_wrapper(image, labels):
+    image_aug = tf.py_function(keras_augmentation, [image], tf.uint8)
+    image_aug.set_shape(image.shape)
+    return image_aug, labels
+
+
+def tf_augmentation(images, labels):
+    images = tf.cast(images, tf.float32)
+    images = tf.image.random_brightness(images, 100)
+    images = tf.image.random_flip_left_right(images)
+    images = tf.image.random_flip_up_down(images)
+    return images, labels
+
+
+def benchmark(dataset, num_epochs=5):
+    import time
+    start_time = time.perf_counter()
+    for epoch_num in range(num_epochs):
+        for sample in dataset:
+            time.sleep(0.01)  # Performing a training step
+    tf.print("Execution time:", time.perf_counter() - start_time)
+
+
+def test_pipeline(dataset):
+    import matplotlib.pyplot as plt
+    dataset_np = dataset.as_numpy_iterator()
+    images, labels = dataset_np.next()
+    for image, label in zip(images, labels):
+        fig = plt.figure()
+        fig.set_size_inches(18, 10, forward=True)
+        plt.imshow(((image + 1) * 127.5).clip(min=0,max=255).astype(np.uint8))
+        plt.title(f"Label: {label}")
+        plt.waitforbuttonpress()
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    import TrainConfig as config
+    config.data_folder = Path(config.data_folder)
+    ds, ds_test = get_data_for_classification_tfds(config)
+    test_pipeline(ds)
+    test_pipeline(ds_test)
+    benchmark(ds, num_epochs=1)  # Pre run to cache DS
+    benchmark(ds)
