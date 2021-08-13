3cfbe58c77d5a84c3f0cd993e9aa235d582993c2
Anders Dahl Henriksen <ahe@proinvent.dk>
2021-08-13 11:15:23 +0200

    -Simplified attention_map.py script

--- Scripts/attention_map.py
+++ Scripts/attention_map.py
@@ -3,16 +3,17 @@
 # From: https://github.com/keisen/tf-keras-vis/blob/master/examples/attentions.ipynb
 
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras import backend as K
-import matplotlib.pyplot as plt
 from matplotlib import cm
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tf_keras_vis.scorecam import Scorecam
+from tf_keras_vis.utils.scores import CategoricalScore
 from tf_keras_vis.saliency import Saliency
-from tf_keras_vis.utils import standardize
-from tf_keras_vis.gradcam import Gradcam
+from tf_keras_vis.gradcam_plus_plus import GradcamPlusPlus
+from tf_keras_vis.utils.model_modifiers import ReplaceToLinear
 import TrainConfig as config
 from Evaluate import get_best_model_path
+from Utils import showimg
 
 MODEL_PATH = get_best_model_path()
 DATA_FOLDER = config.data_folder + '\\Test'
@@ -20,47 +21,32 @@ DATA_FOLDER = config.data_folder + '\\Test'
 preprocess_input = lambda x: x / 127.5 - 1.0
 
 # Load model / modify
-def model_modifier(m):
-    m.layers[-1].activation = tf.keras.activations.linear  # Replace softmax with linear. Somehow required.
-
 model = load_model(str(MODEL_PATH))
 model.summary()
 
 # Load data
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 rescale_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
 data_gen = rescale_gen.flow_from_directory(DATA_FOLDER, batch_size=1, class_mode='categorical', target_size=config.input_shape[:2])
-image, label_one_hot = next(data_gen)
-loss = lambda output: K.mean(output[:, label_one_hot.argmax()])
-X = image
+X, label_one_hot = next(data_gen)
+score = CategoricalScore([label_one_hot.argmax()])
+
+showimg(((X[0].mean(axis=-1) + 1) * 127.5).astype(np.uint8))
 
 # --- Saliency ---
-saliency = Saliency(model, model_modifier)
-saliency_map = saliency(loss, X, smooth_samples=20)
-saliency_map = standardize(saliency_map)
+saliency = Saliency(model, model_modifier=ReplaceToLinear(), clone=True)
+saliency_map = saliency(score, X, smooth_samples=20, smooth_noise=0.20)
+showimg(saliency_map[0] * 255, cmap='jet')
 
-plt.figure()
-plt.imshow(((X[0].mean(axis=-1) + 1) * 127.5).astype(np.uint8))
-plt.show()
-
-plt.figure()
-# plt.imshow((X[0].mean(axis=-1)*255).astype(np.uint8))
-plt.imshow(saliency_map[0] * 255, cmap='jet')
-plt.waitforbuttonpress()
-
-# --- Grad CAM ---
-
-# Create Gradcam object
-gradcam = Gradcam(model, model_modifier)
-
-# Generate heatmap with GradCAM
-cam = gradcam(loss, X)
-cam = standardize(cam)
-
-plt.figure()
+# ---- Grad CAM ---
+gradcam = GradcamPlusPlus(model, model_modifier=ReplaceToLinear(), clone=True)
+cam = gradcam(score, X)
 heatmap = np.uint8(cm.jet(cam[0])[..., :3] * 255)
-# plt.imshow((X[0].mean(axis=-1)*255).astype(np.uint8))
-plt.imshow(heatmap, cmap='jet')
-plt.waitforbuttonpress()
+showimg(heatmap, cmap='jet')
+
+# --- ScoreCAM ---
+scorecam = Scorecam(model, model_modifier=ReplaceToLinear())
+cam = scorecam(score, X, penultimate_layer=-1)
+heatmap = np.uint8(cm.jet(cam[0])[..., :3] * 255)
+showimg(heatmap, cmap='jet')
 
 _ = 'bp'
